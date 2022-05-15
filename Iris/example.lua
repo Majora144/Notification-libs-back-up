0da758b76d69c058ab3472a5375964af27d0dac0
Majora <97760119+Majora144@users.noreply.github.com>
2022-05-15 18:02:07 +0200

    Create example.lua

---
+++ Iris/example.lua
@@ -0,0 +1,26 @@
+ocal Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Majora144/Notification-libs-back-up/main/Iris/main.lua"))()
+
+Notification.WallNotification("God", "Yeet", {
+    MainSettings = {
+        Orientation = "Left",
+        VisibleSize = UDim2.new(0.5, 0, 0.5, 0);
+        HiddenSize  = UDim2.new(0, 0, 0.5, 0),
+        TweenTime   = 0.8
+    },
+})
+
+Notification.Notify("{ Iris }", "Yeet", "rbxassetid://7258714296", {
+    Duration = 7,
+    
+    TitleSettings = {
+        TextXAlignment = Enum.TextXAlignment.Center,
+        Font = Enum.Font.SourceSansSemibold,
+    },
+
+    GradientSettings = {
+        GradientEnabled = false,
+        SolidColorEnabled = true,
+        SolidColor = Color3.fromRGB(124, 83, 240),
+        Retract = true
+    }
+})
